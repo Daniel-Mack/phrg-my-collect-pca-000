5d8a8a6c265f52837527d6ee5057e6ece80f5704
Daniel-Mack <daniel.mack@powerhrg.com>
2018-05-11 09:59:20 -0400

    Built my_collect method

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(empty_array)
+  block = 0
+  collection = []
+    while block < empty_array.length
+      collection << yield(empty_array[block])
+      block+= 1
+    end
+      collection
+end
